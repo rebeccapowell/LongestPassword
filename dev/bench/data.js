ca2b17f24cb12a6bee82f4c7030c739fa94a90cb
github-action-benchmark <github@users.noreply.github.com>
2022-08-04 00:27:40 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 963af01c36a009e9198528201ff98d134c9a8d9e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659571894482,
+  "lastUpdate": 1659572860392,
   "repoUrl": "https://github.com/rebeccapowell/LongestPassword",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "value": 2161.5142996651784,
             "unit": "ns",
             "range": "± 22.603729052184747"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rebecca.powell@edmgroup.com",
+            "name": "Rebecca Powell"
+          },
+          "committer": {
+            "email": "rebecca.powell@edmgroup.com",
+            "name": "Rebecca Powell"
+          },
+          "distinct": true,
+          "id": "963af01c36a009e9198528201ff98d134c9a8d9e",
+          "message": "Altering methd name. Regex from RegEx.",
+          "timestamp": "2022-08-04T02:25:27+02:00",
+          "tree_id": "3bd537621262dc19f9b9d1d1e28e87ca4b19f845",
+          "url": "https://github.com/rebeccapowell/LongestPassword/commit/963af01c36a009e9198528201ff98d134c9a8d9e"
+        },
+        "date": 1659572860024,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "ApplicationBenchmark.Evaluation.SolutionRegexWithOrderedSelect",
+            "value": 22667.07241007487,
+            "unit": "ns",
+            "range": "± 124.51755817440296"
+          },
+          {
+            "name": "ApplicationBenchmark.Evaluation.SolutionRegExWithMax",
+            "value": 21146.318756103516,
+            "unit": "ns",
+            "range": "± 112.57544071996043"
+          },
+          {
+            "name": "ApplicationBenchmark.Evaluation.SolutionIsLetterOrDigit",
+            "value": 2561.326626078288,
+            "unit": "ns",
+            "range": "± 5.962341490935095"
+          },
+          {
+            "name": "ApplicationBenchmark.Evaluation.SolutionStrictIsLetterOrDigit",
+            "value": 1663.269161605835,
+            "unit": "ns",
+            "range": "± 14.5425231787428"
+          },
+          {
+            "name": "ApplicationBenchmark.Evaluation.Solution",
+            "value": 2284.9518445332847,
+            "unit": "ns",
+            "range": "± 4.634054787583003"
           }
         ]
       }
